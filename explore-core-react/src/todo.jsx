19541395fa033d860923bc22484e342f295335c0
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-02-25 21:56:55 +0600

    New JSX code

--- explore-core-react/src/todo.jsx
+++ explore-core-react/src/todo.jsx
@@ -6,10 +6,18 @@
 
 // }
 
+// export default function ToDo({ task, isDone , time = 0}) {
+//   if (isDone) {
+//     return <li>Done: {task} Duration: {time} min</li>;
+//   } else {
+//     return <li>Do Now: {task} Duration: {time} min </li>;
+//   }
+// }
+
+
+
 export default function ToDo({ task, isDone , time = 0}) {
-  if (isDone) {
-    return <li>Done: {task} Duration: {time} min</li>;
-  } else {
-    return <li>Do Now: {task} Duration: {time} min </li>;
-  }
+  
+    return  isDone ? <li>Done: {task} Duration: {time} min</li> :    <li>Do Now: {task} Duration: {time} min </li>;
+  
 }
